afaad63c790e5283ecf56507555983b501d98295
Daniel Hoyte <daniel.hoyte59@gmail.com>
2020-04-30 21:20:56 +0000

    Automatically backed up by Learn

--- lib/car.rb
+++ lib/car.rb
@@ -2,8 +2,11 @@ require_relative "./vehicle.rb"
 
 class Car < Vehicle
 
+<<<<<<< HEAD
 def go
 "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
 end
 
+=======
+>>>>>>> f51f94e35b2beead4c7b2cd5fdc23201f1235f57
 end
